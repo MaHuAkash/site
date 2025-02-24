89a335ffc711924cc0434574a94178d7f537191f
MaHuAkash <198496279+MaHuAkash@users.noreply.github.com>
2025-02-23 17:38:04 -0800

    Include .next and node_modules

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5b0+lOFQHOmUnsUdb5IviMx6TZC28f5FBhZtvOr7oXg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
